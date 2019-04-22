d36cd249b0d9b15827a8a9ddd14c7c53a2e307cb
bciscopanjoj <brayanpanjojcalgua@gmail.com>
2019-04-21 20:31:47 -0600

    Primera Interfaz

---
+++ src/proyectofinalrpg/ProyectoFinalRPG.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalrpg;
+
+import Interfaces.Inicio1;
+
+
+
+/**
+ *
+ * @author braya
+ */
+public class ProyectoFinalRPG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // LLamar Inicio1
+        Inicio1 principal = new Inicio1();
+        principal.show();
+
+  
+    }
+    
+}
